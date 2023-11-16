852b35fa2aeeae4d609433cfd5812d334a1374b4
ashley <ashleychew2000@gmail.com>
2023-11-16 12:25:47 +0800

    new file:   task1/src/sdf/task01/Google.java 	new file:   task1/src/sdf/task01/Main.java

---
+++ task1/src/sdf/task01/Google.java
@@ -0,0 +1,87 @@
+package sdf.task01;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Google {
+
+    private String app;
+    private String category;
+    //crete a linkedlist to store the ratings ->allow us to get the size
+    private List<Double> ratings =  new LinkedList<>();
+    private double highestRating;
+    private double lowestRating;
+    private String highestRapp;
+    private String lowestRapp;
+
+
+    public Google(String app, String category) {
+        this.app = app;
+        this.category =category;
+    }
+
+    public String getApp() {
+        return app;
+    }
+
+    //getters & setters for category
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public List<Double> getRating() {
+        return ratings;
+    }
+
+    public double getHighestRating() {
+        return highestRating;
+    }
+
+    public double getLowestRating() {
+        return lowestRating;
+    }
+
+    public String getHighestRapp() {
+        return highestRapp;
+    }
+
+    public void setHighestRapp(String highestRapp) {
+        this.highestRapp = highestRapp;
+    }
+
+    public String getLowestRapp() {
+        return lowestRapp;
+    }
+
+    public void setLowestRapp(String lowestRapp) {
+        this.lowestRapp = lowestRapp;
+    }
+
+    //function to add ratings into the linkedlist
+    //public void addRatings(){
+    //  
+    //}
+    //
+
+
+
+
+
+    //get average
+    public double avrRating(List <Double> ratings) {
+        int count = ratings.size(); //get the total count for that category
+        int sum = 0;
+        for (int i=0; i<count; i++) {
+            sum +=1;
+        }
+        return sum/count;
+
+
+    }
+
+    
+}
---
+++ task1/src/sdf/task01/Main.java
@@ -0,0 +1,73 @@
+package sdf.task01;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.nio.file.Files;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collector;
+
+public class Main {
+
+    public static final int COL_APP = 0;
+    public static final int COL_NAME = 1;
+    public static final int COL_RATING = 2;
+
+    public static void main(String[] args) throws Exception {
+        
+        //check if filename is provided, else exit program
+        if (args.length <= 0) {
+            System.err.println("Missing google play store csv");
+            System.exit(1);
+        }
+
+        //for counting lines
+        int countline = 0; 
+        try (FileReader fr = new FileReader(args[0])) {
+            BufferedReader br = new BufferedReader(fr);
+
+            //create hashmap to store key and values
+            Map<String, List<Google>> classified = new HashMap<>();
+            //br.readLine() -> reads line by line
+            br.lines()
+                .skip(1) //skip header
+                //remove line with nan values using filter
+                .filter(line -> Objects.nonNull(line))
+                //remove trailing lines and split by comma
+                //map will convert String -> String[]
+                .map(line -> line.trim().split(","))
+                //map the columns
+                .map(columns -> new Google(columns[COL_APP]
+                ,columns[COL_NAME]
+                ,Double.parseDouble(columns[COL_RATING])))
+
+                //group by categories
+                .collect(Collector.groupingBy(cat -> cat.category());
+                
+                //idea is for each category, 
+                //put category and value into hashmap
+                //value is the rating
+                //use .put
+                // PLAN CODE
+                //for (Google g: store) {
+                    
+                //}
+
+                }
+
+
+
+            //printing
+            //for (String cat: classified.keySet()) {
+            //    System.out.printf("%s: %s\n", );
+
+
+            //}
+
+
+
+        }
+
+}
